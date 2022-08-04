e8f7ae967ba7ce750e1b3e2e9e3f7035f9325e32
kleintrevor <trevor.klein@gmail.com>
2022-08-04 02:41:22 -0500

    update to add mutiple enemy support at random

--- script.js
+++ script.js
@@ -4,21 +4,39 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
+const numberOfEnemies = 20;
+const enemiesArray = [];
+
+const enemyImage = new Image();
+enemyImage.src ='images/enemy1.png'
 
 class Enemy {
     constructor(){
-        this.x = 10;
-        this.y = 50;
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
         this.width = 100;
         this.height = 100;
+        this.speed = Math.random() * 4 - 2;
     }
+    update(){
+        this.x+= this.speed;
+        this.y+= this.speed;
+    }
+    draw(){
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+};
+
+for (let i = 0; i < numberOfEnemies; i++){
+    enemiesArray.push(new Enemy());
 }
 
 function animate(){
-    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
-    enemy1.x++
-    ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+   enemiesArray.forEach(enemy => {
+    enemy.update();
+    enemy.draw();  
+   })
     requestAnimationFrame(animate);
 }
-
 animate();
